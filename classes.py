100a722b8299d8da6b5b4fbc3db7fe5e2d333d3f
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-05-28 21:13:43 +1200

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 class Employee:
     
+    num_of_emps = 0
     raise_amount = 1.04
 
     def __init__(self, first, last, pay):
@@ -7,6 +8,8 @@ class Employee:
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
+
+        Employee.num_of_emps += 1
     
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
@@ -21,3 +24,4 @@ emp_2 = Employee(first='Leslie2', last='Alldridge2', pay=2)
 print(emp_1.pay)
 emp_1.apply_raise()
 print(emp_1.pay)
+print(Employee.num_of_emps)
