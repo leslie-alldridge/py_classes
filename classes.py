0fb7328123624d9ff858432de8b018e6aa1b9e7d
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-05-28 21:11:17 +1200

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,27 +1,23 @@
 class Employee:
     
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
+    
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = float(self.pay * self.raise_amount)
 
 
-emp_1 = Employee(first='Leslie', last='Alldridge', pay='123123')
-emp_2 = Employee(first='Leslie2', last='Alldridge2', pay='2')
+emp_1 = Employee(first='Leslie', last='Alldridge', pay=123)
+emp_2 = Employee(first='Leslie2', last='Alldridge2', pay=2)
 
-# print(emp_1)
-# print(emp_2)
-
-# emp_1.first = 'Leslie'
-# emp_1.last = 'Alldridge'
-# emp_1.pay = '123123'
-# emp_1.email = 'test@gmail.com'
-
-# emp_2.first = 'Leslie2'
-# emp_2.last = 'Alldridge2'
-# emp_2.pay = '12'
-# emp_2.email = '22test@gmail.com'
-
-print(emp_1.email)
-print(emp_2.email)
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_1.pay)
