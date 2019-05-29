adeab94ddaa853e692c410b62f6bd85b42c96f59
leslie.alldridge <leslie.alldridge@gmail.com>
2019-05-30 06:01:30 +1200

    super

--- classes.py
+++ classes.py
@@ -1,5 +1,8 @@
+import datetime
+
+
 class Employee:
-    
+
     num_of_emps = 0
     raise_amount = 1.04
 
@@ -10,22 +13,22 @@ class Employee:
         self.email = first + '.' + last + '@company.com'
 
         Employee.num_of_emps += 1
-    
+
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
-    
+
     def apply_raise(self):
-        self.pay = float(self.pay * self.raise_amount)
+        self.pay = int(self.pay * self.raise_amount)
 
     @classmethod
     def set_raise(cls, amount):
         cls.raise_amount = amount
-    
+
     @classmethod
     def from_string(cls, emp_string):
         first, last, pay = emp_string.split('-')
         return cls(first, last, pay)
-    
+
     @staticmethod
     def is_workday(day):
         if day.weekday() == 5 or day.weekday() == 6:
@@ -33,18 +36,18 @@ class Employee:
         return True
 
 
-emp_str_1 = 'Leslie-Alldridge-2042'
+class Developer(Employee):
+    raise_amount = 1.10
 
-emp_1 = Employee.from_string(emp_str_1)
-emp_2 = Employee(first='Leslie2', last='Alldridge2', pay=2)
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
-Employee.set_raise(1.05)
 
-#print(Employee.raise_amount)
-print(emp_1.email)
-print(emp_2.raise_amount)
+dev_2 = Developer(first='Leslie2', last='Alldridge2', pay=20, prog_lang="Java")
 
-import datetime
-my_date = datetime.date(2016, 7, 11)
+dev_2.apply_raise()
 
-print(Employee.is_workday(my_date))
+# print(Employee.raise_amount)
+print(dev_2.prog_lang)
+print(dev_2.raise_amount)
